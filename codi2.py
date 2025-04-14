66fc5c5a0eed27682f2d083ea9f101978869b199
rogercampos006 <1750478@uab.cat>
2025-04-14 10:30:16 +0200

    Update codi2.py

--- codi2.py
+++ codi2.py
@@ -1,1 +1,27 @@
+import networkx as nx
+import random
 
+G = nx.Graph()
+
+def build_lastgraph():
+    G = nx.Graph()
+    with open('lastfm_asia_edges.csv', 'r') as f:
+        next(f)
+        for linia in f:
+            node1, node2 = linia.strip().split(',')
+            G.add_edge(node1, node2)
+    return G
+
+def simulate_coincidence(m,s):
+    G = build_lastgraph()
+    arestes = list(G.edges())
+    for x, y in arestes:
+        pes = random.gauss(m, s)
+        pes = max(0, min(pes, 1))
+        G [x] [y] ['pes'] = round(pes, 3)
+    
+    return G
+
+def  how_many_cliques(n,m,s):
+    G = simulate_coincidence(m, s)
+    arestes = list(G.edges())
