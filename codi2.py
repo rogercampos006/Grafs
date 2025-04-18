f96fe0272cecaa37c3097ed41abec09c940c663b
rogercampos006 <1750478@uab.cat>
2025-04-18 10:19:15 +0200

    Update codi2.py

--- codi2.py
+++ codi2.py
@@ -15,20 +15,19 @@ def build_lastgraph():
 
 def simulate_coincidence(m,s):
     G = build_lastgraph()
-    arestes = list(G.edges())
-    for x, y in arestes:
+    for x, y in G.edges():
         pes = random.gauss(m, s)
         pes = max(0, min(pes, 1))
-        G [x] [y] ['pes'] = round(pes, 3)
+        G [x][y]['pes'] = round(pes, 3)
     
     return G
 
 def  how_many_cliques(n,m,s):
     G = simulate_coincidence(m, s)
     H = nx.Graph()
-    for u, v in G.nodes():
+    for u, v, data in G.edges(data = True):
         if G [u][v] ['pes'] > n:
-            H.add_edge ([u], [v])
+            H.add_edge (u, v)
     cliques = list(nx.find_cliques(H))
     comptador = Counter(len(c) for c in cliques)
     return comptador
